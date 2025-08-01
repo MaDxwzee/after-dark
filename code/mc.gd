bab5e90e38d404b547005cc14acf14eb3be39281
MaD <madrbx7@gmail.com>
2025-08-01 18:25:26 +0100

    mouaad lgrbouz HHH

--- code/mc.gd
+++ code/mc.gd
@@ -4,51 +4,40 @@ extends CharacterBody2D
 @export var run_speed := 200
 @export var acceleration := 1000.0
 @export var friction := 800.0
-@export var gravity := 1200.0
-@export var jump_force := -400.0
+@export var gravity := 1000.0
+@export var jump_force := -500.0
 
 var last_direction := Vector2.RIGHT
-var jumping := false  # 👈 added for camera to read
-
 @onready var anim_sprite := $AnimatedSprite2D
 
 func _physics_process(delta):
-	var input_vector = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		0
-	).normalized()
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 
-	var is_running := Input.is_action_pressed("run")
-	var current_speed := run_speed if is_running else walk_speed
+	var is_running = Input.is_action_pressed("run")
+	var speed = run_speed if is_running else walk_speed
 
-	# Apply gravity
-	velocity.y += gravity * delta
-
-	# Jump
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y = jump_force
-		jumping = true  # 👈 jumping now
+	else:
+		velocity.y += gravity * delta
 
-	# Check landing
-	if is_on_floor() and velocity.y >= 0:
-		jumping = false  # 👈 landed
-
-	# Horizontal movement
 	if input_vector.x != 0:
-		velocity.x = move_toward(velocity.x, input_vector.x * current_speed, acceleration * delta)
-
-		# Animation and flip
-		if is_running:
-			anim_sprite.play("run")
-		else:
-			anim_sprite.play("walk")
-
-		anim_sprite.flip_h = input_vector.x < 0
-		last_direction = input_vector
+		velocity.x = move_toward(velocity.x, input_vector.x * speed, acceleration * delta)
+		last_direction.x = input_vector.x
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+	if not is_on_floor():
+		if velocity.y < 0:
+			anim_sprite.play("jump")
+		else:
+			anim_sprite.play("fall")
+	elif input_vector.x != 0:
+		anim_sprite.play("run" if is_running else "walk")
+	else:
 		anim_sprite.play("idle")
-		if last_direction.x != 0:
-			anim_sprite.flip_h = last_direction.x < 0
+
+	anim_sprite.flip_h = last_direction.x < 0
 
 	move_and_slide()
